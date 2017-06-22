344e9d14667ad11c80eb3a961409f6cb79d06220
foresitegroup <mark@foresitegrp.com>
2017-06-22 16:28:14 -0500

    2017-06-22

--- wp-content/themes/bublr/front-page.php
+++ wp-content/themes/bublr/front-page.php
@@ -61,6 +61,65 @@ while ( have_posts() ) : the_post();
     </div>
   </div>
 
+  <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/inc/swipebox/swipebox.css">
+  <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/inc/swipebox/jquery.swipebox.min.js"></script>
+  <script type="text/javascript">
+    jQuery(document).ready(function() {
+      jQuery(".video").swipebox({autoplayVideos: true});
+    });
+  </script>
+
+  <div id="how">
+    <div class="site-width">
+      <?php echo do_shortcode('[insert page="how-it-works" display="content"]'); ?>
+    </div>
+  </div>
+
+  <div id="explore" class="cf">
+    <div class="main">
+      <div class="header">
+        EXPLORE BY BUBLR
+      </div>
+
+      MAIN SECTION
+    </div>
+
+    <div class="sidebar">
+      <div class="all">
+        <a href="#" class="arrow">EXPLORE ALL</a>
+      </div>
+
+      <div id="weather">
+        <?php
+        if (strtotime('-5 minutes') > filemtime('darksky.json')) {
+          include_once "inc/fintoozler.php";
+          $json = file_get_contents('https://api.darksky.net/forecast/' . $dsapi . '/43.0389,-87.9065?exclude=minutely,daily,alerts,flags', false, stream_context_create(array('ssl' => array('verify_peer' => false, 'verify_peer_name' => false))));
+          file_put_contents('darksky.json',$json);
+        } else {
+          $json = file_get_contents('darksky.json');
+        }
+
+        $obj = json_decode($json, true);
+        $cc = $obj['currently'];
+        $hc = $obj['hourly'];
+        ?>
+        
+        <div class="icon"><canvas id="<?php echo $cc['icon']; ?>"></canvas></div>
+        <h3><?php echo round($cc['temperature']); ?>&deg;</h3><br>
+        <h4><?php echo $cc['summary']; ?></h4>
+        <?php echo $hc['summary']; ?>
+
+        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/inc/skycons.js"></script>
+        <script>
+          var icons = new Skycons({"color": "white"}),
+          list  = ["clear-day", "clear-night", "partly-cloudy-day", "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind", "fog" ], i;
+          for(i = list.length; i--;) icons.set(list[i], list[i]);
+          icons.play();
+        </script>
+      </div>
+    </div>
+  </div>
+
   <?php
 endwhile;
 
