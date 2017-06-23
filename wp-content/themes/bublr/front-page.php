1ceab20ac4739fd80fb68ee15c0ed8c9d74d2aa5
foresitegroup <mark@foresitegrp.com>
2017-06-23 13:22:30 -0500

    2017-06-23

--- wp-content/themes/bublr/front-page.php
+++ wp-content/themes/bublr/front-page.php
@@ -77,47 +77,99 @@ while ( have_posts() ) : the_post();
 
   <div id="explore" class="cf">
     <div class="main">
-      <div class="header">
+      <div class="background-text">COMMUNITY DRIVEN</div>
+
+      <div class="header content-width">
         EXPLORE BY BUBLR
       </div>
+      
+      <?php
+      $main_post = get_posts(array('posts_per_page' => 1));
+      foreach ($main_post as $post) : setup_postdata($post);
+      ?>
+        <div id="latest-post" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id()); ?>);">
+          <div class="content-width">
+            <div class="date">
+              <i class="fg fg-clock"></i>
+              <?php the_date(); ?>
+              &bull;
+              <?php
+              foreach (get_the_category() as $category) $cat_names[] = $category->name;
+              echo implode(', ', $cat_names);
+              ?>
+            </div>
+
+            <?php the_title('<h3>', '</h3>'); ?><br>
+
+            <?php
+            $split = preg_split('/(\.|\!|\?)/', get_the_content(), 3, PREG_SPLIT_DELIM_CAPTURE);
+            echo strip_tags(implode('', array_slice($split, 0, 4)));
+            ?><br>
+
+            <a href="<?php the_permalink(); ?>" class="button">READ STORY</a>
+          </div>
+        </div>
+      <?php
+      endforeach; 
+      wp_reset_postdata();
+      ?>
 
       MAIN SECTION
+
+      <div style="width: 10px; height: 1000px; outline: 1px solid red;"></div>
     </div>
 
     <div class="sidebar">
       <div class="all">
-        <a href="#" class="arrow">EXPLORE ALL</a>
+        <a href="explore" class="arrow">EXPLORE ALL</a>
       </div>
+      
+      <div class="sidebar-shadow">
+        <div id="weather">
+          <h5>MILWAUKEE</h5>
 
-      <div id="weather">
-        <?php
-        if (strtotime('-5 minutes') > filemtime('darksky.json')) {
-          include_once "inc/fintoozler.php";
-          $json = file_get_contents('https://api.darksky.net/forecast/' . $dsapi . '/43.0389,-87.9065?exclude=minutely,daily,alerts,flags', false, stream_context_create(array('ssl' => array('verify_peer' => false, 'verify_peer_name' => false))));
-          file_put_contents('darksky.json',$json);
-        } else {
-          $json = file_get_contents('darksky.json');
-        }
+          <?php
+          if (strtotime('-5 minutes') > filemtime('darksky.json')) {
+            include_once "inc/fintoozler.php";
+            $json = file_get_contents('https://api.darksky.net/forecast/' . $dsapi . '/43.0389,-87.9065?exclude=minutely,daily,alerts,flags', false, stream_context_create(array('ssl' => array('verify_peer' => false, 'verify_peer_name' => false))));
+            file_put_contents('darksky.json',$json);
+          } else {
+            $json = file_get_contents('darksky.json');
+          }
 
-        $obj = json_decode($json, true);
-        $cc = $obj['currently'];
-        $hc = $obj['hourly'];
-        ?>
-        
-        <div class="icon"><canvas id="<?php echo $cc['icon']; ?>"></canvas></div>
-        <h3><?php echo round($cc['temperature']); ?>&deg;</h3><br>
-        <h4><?php echo $cc['summary']; ?></h4>
-        <?php echo $hc['summary']; ?>
+          $obj = json_decode($json, true);
+          $cc = $obj['currently'];
+          $hc = $obj['hourly'];
+          ?>
+          
+          <div class="icon"><canvas id="<?php echo $cc['icon']; ?>"></canvas></div>
+          <h3><?php echo round($cc['temperature']); ?>&deg;</h3><br>
+          <h4><?php echo $cc['summary']; ?></h4>
+          <?php echo $hc['summary']; ?><br>
+          <br>
 
-        <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/inc/skycons.js"></script>
-        <script>
-          var icons = new Skycons({"color": "white"}),
-          list  = ["clear-day", "clear-night", "partly-cloudy-day", "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind", "fog" ], i;
-          for(i = list.length; i--;) icons.set(list[i], list[i]);
-          icons.play();
-        </script>
-      </div>
-    </div>
+          <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/inc/skycons.js"></script>
+          <script>
+            var icons = new Skycons({"color": "white"}),
+            list  = ["clear-day", "clear-night", "partly-cloudy-day", "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind", "fog" ], i;
+            for(i = list.length; i--;) icons.set(list[i], list[i]);
+            icons.play();
+          </script>
+
+          <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+        </div> <!-- END #weather -->
+
+        <?php $quote = get_posts(array('name' => 'home-page-quote', 'post_type' => 'page')); ?>
+        <div id="home-quote">
+          <div class="image" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($quote[0]->ID)); ?>);"></div>
+
+          <div class="text">
+            <?php echo do_shortcode('[insert page="'.$quote[0]->ID.'" display="content"]'); ?>
+          </div>
+        </div>
+
+      </div> <!-- END .sidebar-shadow -->
+    </div> <!-- END .sidebar -->
   </div>
 
   <?php
