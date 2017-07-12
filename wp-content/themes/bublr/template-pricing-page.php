8db2eb0bb4abe93a1b4565ae42bf688a8203d750
foresitegroup <mark@foresitegrp.com>
2017-07-12 17:28:53 -0500

    2017-07-12

--- wp-content/themes/bublr/template-pricing-page.php
+++ wp-content/themes/bublr/template-pricing-page.php
@@ -2,20 +2,24 @@
 /* Template Name: Pricing */
 
 get_header();
+
+$header = get_posts(array('name' => 'pricing-header', 'post_type' => 'page'));
 ?>
 
-<?php
-if ( have_posts() ) :
-	while ( have_posts() ) : the_post();
-    ?>
-    
-    <?php the_content(); ?>
+<div id="pricing-header">
+  <div class="image" style="background-image: url(<?php echo wp_get_attachment_url(get_post_thumbnail_id($header[0]->ID)); ?>);"></div>
 
-    <?php
-	endwhile;
-else : // I'm not sure it's possible to have no posts when this page is shown, but WTH.
-	get_template_part( 'template-parts/post/content', 'none' );
-endif;
+  <div class="site-width">
+    <div class="text">
+      <?php the_title('<h1>', '</h1>'); echo do_shortcode('[insert page="'.$header[0]->ID.'" display="content"]'); ?>
+    </div>
+  </div>
+</div>
+
+<?php
+while ( have_posts() ) : the_post();
+  the_content();
+endwhile;
 ?>
 
 <?php get_footer(); ?>
